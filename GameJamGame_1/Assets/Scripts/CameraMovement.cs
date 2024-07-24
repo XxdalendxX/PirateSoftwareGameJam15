eacbfb16382cbf647f6961866988ad6647e558d7
DESKTOP-LLJ6GNE\connor <connordr112@gmail.com>
2024-07-24 17:41:39 +1000

    Camera Movement done

---
+++ GameJamGame_1/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] float distancefFromPlayer;
+    [SerializeField] Transform target;
+    [SerializeField] float camResetTimer;
+
+    [Space]
+
+    [SerializeField] float lerp;
+    [SerializeField] float resetCooldown = 0;
+    [SerializeField] bool awayFromPlayer = false;
+    [SerializeField] float targetX;
+    [SerializeField] float camX;
+    [SerializeField] float distanceToPlayer;
+
+
+
+
+    private void Update()
+    {
+        targetX = target.position.x;
+        camX = transform.position.x;
+
+        if (awayFromPlayer)
+            ResetTimerHandler();
+
+        transform.position = new Vector3(targetX + lerp, transform.position.y, transform.position.z);
+    }
+
+    public void LerpCamera(PlayerDirection direction)
+    {
+        if (direction == PlayerDirection.left)
+        {
+            lerp = Mathf.Lerp(lerp, -distancefFromPlayer, (1.5f * Time.deltaTime));
+        }
+        else if (direction == PlayerDirection.right)
+        {
+            lerp = Mathf.Lerp(lerp, distancefFromPlayer, (1.5f * Time.deltaTime));
+        }
+    }
+
+    public void ReturnToTarget()
+    {
+        distanceToPlayer = lerp - targetX;
+        distanceToPlayer = targetX - camX;
+        lerp = Mathf.Lerp(lerp, distanceToPlayer, (1.5f * Time.deltaTime));
+    }
+
+    public void SetCooldownTimer()
+    {
+        resetCooldown = camResetTimer;
+        awayFromPlayer = true;
+    }
+
+    void ResetTimerHandler()
+    {
+        bool condition_1 = resetCooldown <= 0;
+        bool condition_2 = awayFromPlayer;
+        bool condition_3 = transform.position.x == target.position.x;
+
+        if (!condition_1)
+            resetCooldown -= Time.deltaTime;
+
+        if (condition_1 && !condition_3)
+        {
+            ReturnToTarget();
+        }
+
+        if (condition_1 && condition_2 && condition_3)
+        {
+            awayFromPlayer = false;
+            resetCooldown = 0;
+        }
+    }
+}
--- GameJamGame_1/Assets/Scripts/PlayerMovement.cs
+++ GameJamGame_1/Assets/Scripts/PlayerMovement.cs
@@ -2,34 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerDirection
+{
+    none,
+    left,
+    right,
+}
+
 public class PlayerMovement : MonoBehaviour
 {
-    public Transform playerBody;
+    Transform player;
     public float playerSpeed = 5f;
-    public Rigidbody2D rb;
+    Rigidbody2D rb;
 
-    [HideInInspector] public enum PlayerDirection
-    {
-        none,
-        left,
-        right,
-    }
+    [SerializeField] Transform playerBody;
+    [SerializeField] CameraMovement cam;
+
     public PlayerDirection walkDirection = PlayerDirection.none;
     [HideInInspector] public bool interacting;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        player = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        MovementCheck();
+        PlayerInputCheck();
         MovePlayer();
     }
 
-    void MovementCheck()
+    void PlayerInputCheck()
     {
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
@@ -42,13 +47,11 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
         {
-            Debug.Log("player has stopped travelling left");
             if (walkDirection == PlayerDirection.left)
                 walkDirection = PlayerDirection.none;
         }
         else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
         {
-            Debug.Log("player has stopped travelling right");
             if (walkDirection == PlayerDirection.right)
                 walkDirection = PlayerDirection.none;
         }
@@ -62,12 +65,20 @@ public class PlayerMovement : MonoBehaviour
         if (walkDirection == PlayerDirection.left)
         {
             rb.velocity = Vector2.left * playerSpeed;
-        }
+            playerBody.localRotation = Quaternion.Euler(0, 180, 0);
+            cam.LerpCamera(walkDirection);
+            cam.SetCooldownTimer();
+        } //move player left
         else if (walkDirection == PlayerDirection.right)
         {
             rb.velocity = Vector2.right * playerSpeed;
-        }
+            playerBody.localRotation = Quaternion.identity;
+            cam.LerpCamera(walkDirection);
+            cam.SetCooldownTimer();
+        } // move player right
         else if (walkDirection == PlayerDirection.none && rb.velocity != Vector2.zero)
             rb.velocity = Vector2.zero;
     }
+
+
 }
