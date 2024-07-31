fad6f1aac2c8046498bba58be550bf7a90ba3aa6
DESKTOP-LLJ6GNE\connor <connordr112@gmail.com>
2024-07-31 23:11:20 +1000

    GameJam Game Finished and submitted

--- GameJamGame_1/Assets/Scripts/UI/MainMenuScript.cs
+++ GameJamGame_1/Assets/Scripts/UI/MainMenuScript.cs
@@ -5,9 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public Transform controlsText;
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void Controls()
+    {
+        controlsText.gameObject.SetActive(!controlsText.gameObject.activeSelf);
     }
 
     public void ExitGame()
